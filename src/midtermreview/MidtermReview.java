8cf90f594793f6360c998ed54241104899e1bbef
John <chejiaha@sheridancollege.ca>
2019-03-03 20:55:48 -0500

    First commit

---
+++ src/midtermreview/MidtermReview.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreview;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author John
+ */
+public class MidtermReview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("enter a word");
+        String str = input.nextLine();
+        System.out.println("the word is " + str);
+    }
+    
+}
